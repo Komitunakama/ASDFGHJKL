cd9cc90694c18897babb5ecac61044a6ddadf830
Komitunakama <Gutierres.Freeman@protonmail.com>
2026-05-24 16:37:01 -0300

    UIuydijdhjghsus

--- configuration.nix
+++ configuration.nix
@@ -155,6 +155,8 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  services.flatpak.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
