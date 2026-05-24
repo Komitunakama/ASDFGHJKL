36c94e9bfa710fb05a58ca2983a13493414e035c
Komitunakama <Gutierres.Freeman@protonmail.com>
2026-05-24 12:27:30 -0300

    first commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Sistema NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos =
+      nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./configuration.nix
+        ];
+      };
+  };
+}
