18e0a66e6d950f5d80bdfedff8e4114ecc4b9864
Komitunakama <Gutierres.Freeman@protonmail.com>
2026-05-24 13:13:47 -0300

    Atualização da versão

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Sistema NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs }: {
