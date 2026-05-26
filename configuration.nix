bc560f0319753a03eac44f720536950f957ab934
Komitunakama <Gutierres.Freeman@protonmail.com>
2026-05-25 21:11:20 -0300

    Sway yay

--- configuration.nix
+++ configuration.nix
@@ -49,11 +49,13 @@
   };
 
   # Enable the X11 windowing system.
+  
   services.xserver.enable = true;
-
+  
   # Enable the XFCE Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
+  programs.sway.enable = true;
 
   environment.xfce.excludePackages = with pkgs.xfce; [
   ristretto
